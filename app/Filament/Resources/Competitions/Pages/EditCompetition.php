d6f2f8f03070ce14247d68c7959c2f9a73c59a66
Gilles Audemard <gilles.audemard@univ-artois.fr>
2026-07-07 14:29:34 +0200

    done

--- app/Filament/Resources/Competitions/Pages/EditCompetition.php
+++ app/Filament/Resources/Competitions/Pages/EditCompetition.php
@@ -2,7 +2,10 @@
 
 namespace App\Filament\Resources\Competitions\Pages;
 
+use App\Filament\Pages\MissingResults;
 use App\Filament\Resources\Competitions\CompetitionResource;
+use App\Models\Competition;
+use Filament\Actions\Action;
 use Filament\Actions\DeleteAction;
 use Filament\Resources\Pages\EditRecord;
 
@@ -14,6 +17,15 @@ class EditCompetition extends EditRecord
     {
         return [
             DeleteAction::make(),
+        ];
+    }
+
+    protected function getFormActions(): array
+    {
+        return [
+            ...parent::getFormActions(),
+            Action::make('Missing')
+                ->url(fn (Competition $record) => MissingResults::getUrl(['competition' => $record->id])),
         ];
     }
 }
