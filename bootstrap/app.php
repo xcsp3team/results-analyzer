5030ef782934cc2f99f92362f52d0a7934c50732
Gilles Audemard <gilles.audemard@univ-artois.fr>
2026-07-07 17:00:44 +0200

    multi environment

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -5,7 +5,7 @@ use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
 use Illuminate\Http\Request;
 
-return Application::configure(basePath: dirname(__DIR__))
+$app = Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
         web: __DIR__.'/../routes/web.php',
         api: __DIR__.'/../routes/api.php',
@@ -20,3 +20,13 @@ return Application::configure(basePath: dirname(__DIR__))
             fn (Request $request) => $request->is('api/*'),
         );
     })->create();
+
+
+if(!empty($_SERVER['HTTP_HOST']) && str_contains($_SERVER['HTTP_HOST'], 'localhost') === false &&  str_contains($_SERVER['HTTP_HOST'], '127.0.0.1') === false) {
+    $domain = $_SERVER['HTTP_HOST'];
+    if (isset($domain))
+        $app->loadEnvironmentFrom('.env.'.$domain);
+
+}
+return $app;
+
