3df98fcc11a4510c7516e21e8f918f1568a91039
Gilles Audemard <gilles.audemard@gmail.com>
2026-08-31 18:05:29 +0200

    Summary COP alpha

--- app/Livewire/Summary/SummaryCop.php
+++ app/Livewire/Summary/SummaryCop.php
@@ -9,7 +9,6 @@ class SummaryCop extends AbstractSummary
 {
     const int NAME = 0;
     const int SCORE = 1;
-
     const int OPTIMUM = 2;
     const int BB1 = 3;
     const int BB2 = 4;
@@ -22,6 +21,7 @@ class SummaryCop extends AbstractSummary
         parent::mount($filters, $evaluation, $selected_solvers);
         $this->header_summary = [
             new DataHeader("Solver", "left"),
+            new DataHeader("Score"),
             new DataHeader("#Opt"),
             new DataHeader("#BB1"),
             new DataHeader("#BB2"),
@@ -33,13 +33,20 @@ class SummaryCop extends AbstractSummary
     public function best_score($benchmark_id, $type)
     {
         $all_results = $this->evaluation->all_results_cop($this->filters->time_limit);
-        $tmp = (object)["bound" => null, "optimum" => false];
+        $tmp = (object)["bound" => null, "optimum" => false, "unsat" => false];
         foreach ($this->selected_solvers as $id) {
             $selectedSolver = $this->solvers[$id];
             $data = $all_results[$selectedSolver->id][$benchmark_id];
+            if ($data->bound == null) continue;
+            if ($tmp->bound == null || ($type == "MAXIMIZE" && $tmp->bound < $data->bound) || ($type == "MINIMIZE" && $tmp->bound > $data->bound)) {
+                $tmp->bound = $data->bound;
+                if ($data->time != -1 && $data->time <= $this->filters->time_limit) {
+                    $tmp->optimum = true;
+                    return $tmp;
+                }
+            }
         }
-            return $tmp;
-        }
+        return $tmp;
     }
 
     public function create_summary()
@@ -53,7 +60,7 @@ class SummaryCop extends AbstractSummary
                 $tmp[] = new Data();
             $tmp[self::NAME]->value = $selectedSolver->name . " " . $selectedSolver->version;
             foreach ($this->evaluation->benchmarks as $benchmark) {
-                if ($this->filters->is_filtered($benchmark))
+                if ($this->filters->is_filtered($benchmark) || $benchmark->status == "UNKNOWN")
                     continue;
                 $data = $all_results[$selectedSolver->id][$benchmark->id];
                 if ($data->unsupported) {
@@ -66,13 +73,17 @@ class SummaryCop extends AbstractSummary
                     $type = "MAXIMIZE";
                 else
                     $type = "MINIMIZE";
+                if ($benchmark->status == "UNSAT") {
+                    $tmp[self::UNSAT]->value += 1;
+                    continue;
+                }
 
                 $best_score = $this->best_score($benchmark->id, $type);
                 if ($best_score->bound == null)
                     continue;
                 if (($type == "MAXIMIZE" && $best_score->bound > $data->bound) || ($type == "MINIMIZE" && $best_score->bound < $data->bound))
                     continue;
-                if ($data->time <= $this->filters->time_limit) {
+                if ($data->time != -1 && $data->time <= $this->filters->time_limit) {
                     $tmp[self::OPTIMUM]->value++;
                     continue;
                 }
@@ -80,7 +91,10 @@ class SummaryCop extends AbstractSummary
                     $tmp[self::BB1]->value++;
                 else $tmp[self::BB2]->value++;
             }
+
+            $tmp[self::SCORE]->value = $tmp[self::OPTIMUM]->value + $tmp[self::BB1]->value + $tmp[self::BB2]->value / 2 + $tmp[self::UNSAT]->value;
             $this->summary[] = $tmp;
+
         }
     }
 }
