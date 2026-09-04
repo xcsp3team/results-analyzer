706267fc1d392b3a1b4d183486d0ac7700cf62b5
Gilles Audemard <gilles.audemard@gmail.com>
2026-09-04 15:55:58 +0200

    migration of old DB done

--- app/Console/Commands/ExportNewDB.php
+++ app/Console/Commands/ExportNewDB.php
@@ -3,6 +3,8 @@
 namespace App\Console\Commands;
 
 use App\Livewire\Evaluation;
+use App\Models\Benchmark;
+use App\Models\Result;
 use App\Models\Solver;
 use Illuminate\Console\Attributes\Description;
 use Illuminate\Console\Attributes\Signature;
@@ -22,7 +24,7 @@ class ExportNewDB extends Command
         $host = env('DB_HOST');
         $user = env('DB_USERNAME');
         $passwd = env('DB_PASSWORD');
-        $db = "old_xcsp26"; //$this->ask('Name of the old database?');
+        $db = $this->ask('Name of the old database?');
 
         try {
             $pdo = new PDO("mysql:host=$host;dbname=$db", $user, $passwd, array(PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES utf8"));
@@ -61,18 +63,80 @@ class ExportNewDB extends Command
 
 
             if ($competition['type'] == "cop") {
+                $query = $pdo->query("SELECT * FROM benchmarks_cop WHERE competition_id=$competition_id");
+                $query->setFetchMode(\PDO::FETCH_ASSOC);
+                $benchmarks = $query->fetchAll();
+                foreach ($benchmarks as $benchmark) {
+                    $b = new Benchmark();
+                    $b->name = $benchmark["name"];
+                    $b->fullname = $benchmark["fullname"];
+                    $b->family = $benchmark["family"];
+                    $b->nb_variables = $benchmark["nb_variables"];
+                    $b->nb_clauses = $benchmark["nb_constraints"];
+                    $b->info_domains = $benchmark["info_domains"];
+                    $b->info_constraints = $benchmark["info_constraints"];
+                    $b->useless_vars = $benchmark["useless_vars"];
+                    $b->type = $benchmark["type"];
+                    $b->evaluation_id = $benchmark["competition_id"];
+                    $b->save();
+
+
+                    $benchmark_id = $benchmark["id"];
+                    $query = $pdo->query("SELECT * FROM results_cop WHERE benchmark_id=$benchmark_id");
+                    $results = $query->fetchAll();
+                    foreach ($results as $result) {
+                        $r = new Result();
+                        $r->benchmark_id = $b->id;
+                        $r->solver_id = $result["solver_id"];
+                        $r->bounds = $result["bounds"];
+                        $r->status = "SAT";
+                        if($result['time'] != -1) {
+                            if($result['bounds'] == '[]')
+                                $r->status = "UNSAT";
+                            else
+                                $r->status = "OPTIMUM";
+                        } else {
+                            if($result['bounds'] == '[]')
+                                $r->status = "UNKNOWN";
+                        }
+                        $r->bug = $result["bug"];
+                        $r->unsupported = $result["unsupported"];
+                        $r->time = $result["time"];
+                        $r->save();
+                    }
+                }
             } else {
                 $query = $pdo->query("SELECT * FROM benchmarks WHERE competition_id=$competition_id");
                 $query->setFetchMode(\PDO::FETCH_ASSOC);
                 $benchmarks = $query->fetchAll();
                 foreach ($benchmarks as $benchmark) {
-                    $benchmark["evaluation_id"] = $competition_id;
-                    unset($benchmark["competition_id"]);
-                    unset($benchmark["created_at"]);
-                    unset($benchmark["updated_at"]);
-                    unset($benchmark["status"]);
-                    DB::insert("INSERT INTO benchmarks(id,name,fullname,family, nb_variables,nb_clauses,info_domains,info_constraints,useless_vars,evaluation_id) " .
-                                      "values(:id,:name,:fullname,:family,:evaluation_id,:nb_variables,:nb_clauses,:info_domains,:info_constraints,:useless_vars, :evaluation_id)", $benchmark);
+                    $b = new Benchmark();
+                    $b->name = $benchmark["name"];
+                    $b->fullname = $benchmark["fullname"];
+                    $b->family = $benchmark["family"];
+                    $b->nb_variables = $benchmark["nb_variables"];
+                    $b->nb_clauses = $benchmark["nb_clauses"];
+                    $b->info_domains = $benchmark["info_domains"];
+                    $b->info_constraints = $benchmark["info_constraints"];
+                    $b->useless_vars = $benchmark["useless_vars"];
+                    $b->evaluation_id = $benchmark["competition_id"];
+                    $b->save();
+
+
+                    // Register all results
+                    $benchmark_id = $benchmark["id"];
+                    $query = $pdo->query("SELECT * FROM results WHERE benchmark_id=$benchmark_id");
+                    $results = $query->fetchAll();
+                    foreach ($results as $result) {
+                        $r = new Result();
+                        $r->benchmark_id = $b->id;
+                        $r->solver_id = $result["solver_id"];
+                        $r->status = $result["status"];
+                        $r->bug = $result["bug"];
+                        $r->unsupported = $result["unsupported"];
+                        $r->time = $result["time"];
+                        $r->save();
+                    }
                 }
             }
 
