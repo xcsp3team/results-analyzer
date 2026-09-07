6590f3e4cf5431c19c26fcc2de100216674ea5fc
audemard <gilles.audemard@univ-artois.fr>
2026-09-07 15:22:50 +0200

    improve radars

--- resources/views/livewire/radar.blade.php
+++ resources/views/livewire/radar.blade.php
@@ -3,20 +3,21 @@
 
     <div class="grid lg:grid-cols-3 md:grid-cols-2 gap-y-2">
         @foreach($families as $name => $family)
-            <div class="flex items-center flex-col">
-                <h3 class="text-xl dark:text-gray-400 text-xs">{{$name}}</h3>
+            <div class="">
+                <h3 class="text-center text-lg font-medium dark:text-gray-400 relative top-10">{{$name}}</h3>
                 <div wire:ignore x-data="{
     chart: null,
     init() {
         this.chart = new ApexCharts(this.$refs.{{$name}}, {
-            chart: { type: 'radar', height: '300px',
+            chart: { type: 'radar', height: 400, size: 500,
             toolbar: {
                 show: false,
             }},
             stroke: { width: 1 },
+            tooltip: {intersect: false},
             markers: { size: 2 },
             series: @js([['name' => $family->name, 'data' => $family->data]]),
-            xaxis: { categories: @js($solvers_name) }
+            xaxis: { categories: @js($solvers_name) },
         });
         this.chart.render();
         Livewire.on('radar-{{$name}}-updated', (event) => {
